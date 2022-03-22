317a52772805cae0124056e922dcd0c675387939
WssIDs <wssids49@gmail.com>
2022-03-22 20:18:08 +0300

    Add project files.

---
+++ FileCloud/FileCloud.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FileCloudClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FileCloud/FileCloud.Client/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FileCloudClient
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
