bd9a0853c2ad37c8c8588c7e9668169f1c627286
WssIDs <wssids49@gmail.com>
2022-03-30 15:56:48 +0300

    Added EF Microsoft Identity Context

---
+++ FileCloud.Data/Entities/Role.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FileCloud.Data.Entities
+{
+    public class Role : IdentityRole<Guid>
+    {
+    }
+}
---
+++ FileCloud.Data/Store/FileCloudDbContext.cs
@@ -0,0 +1,21 @@
+﻿using FileCloud.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileCloud.Data.Store
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FileCloudDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        public FileCloudDbContext(DbContextOptions<FileCloudDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
