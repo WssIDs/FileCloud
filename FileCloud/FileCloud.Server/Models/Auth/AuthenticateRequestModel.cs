711469cea7b1be7301f3188a58a30e9437266dce
WssIDs <wssids49@gmail.com>
2022-04-06 00:18:30 +0300

    Added Auth Request and Response models

---
+++ FileCloud/FileCloud.Server/Models/Auth/AuthenticateRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FileCloud.Server.Models.Auth
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AuthenticateRequestModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ FileCloud/FileCloud.Server/Models/Auth/AuthenticateResponseModel.cs
@@ -0,0 +1,21 @@
+﻿namespace FileCloud.Server.Models.Auth
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AuthenticateResponseModel
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string JwtToken { get; set; }
+    }
+}
