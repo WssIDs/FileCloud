e228bd6f0f7614424823d4af240aeb8aa1c3d41c
WssIDs <wssids49@gmail.com>
2022-04-18 12:34:20 +0300

    Added InitValidator for Form Component

---
+++ FileCloud/FileCloud.Client/Validation/InitialValidator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCloudClient.Validation
+{
+    public class InitialValidator : ComponentBase
+    {
+        // Get the EditContext from the parent component (EditForm)
+        [CascadingParameter]
+        private EditContext CurrentEditContext { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            CurrentEditContext?.Validate();
+        }
+    }
+}
