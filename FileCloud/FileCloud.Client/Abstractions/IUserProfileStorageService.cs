1e823e8df09dbedd24f5635b673e62ad9bff33f8
WssIDs <wssids49@gmail.com>
2022-04-22 20:57:34 +0300

    Added User Profile Service for storage user data

---
+++ FileCloud/FileCloud.Client/Abstractions/IUserProfileStorageService.cs
@@ -0,0 +1,17 @@
+﻿namespace FileCloudClient.Abstractions
+{
+    public interface IUserProfileStorageService
+    {
+        Guid Id { get; set; }
+
+        string FirstName { get; set; }
+
+        string LastName { get; set; }
+
+        string UserName { get; set; }
+
+        string Token { get; set; }
+
+        DateTime TokenExpired { get; set; }
+    }
+}
