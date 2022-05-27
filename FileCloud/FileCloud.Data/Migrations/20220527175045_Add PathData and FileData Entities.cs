ebfef92ca10db245bcf237456f915baf564e59f9
WssIDs <wssids49@gmail.com>
2022-05-27 20:53:15 +0300

    Added migration

---
+++ FileCloud/FileCloud.Data/Migrations/20220527175045_Add PathData and FileData Entities.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileCloud.Data.Migrations
+{
+    public partial class AddPathDataandFileDataEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Paths",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Root = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PathDataId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paths", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Paths_Paths_PathDataId",
+                        column: x => x.PathDataId,
+                        principalTable: "Paths",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrginalFileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PathId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Files_Paths_PathId",
+                        column: x => x.PathId,
+                        principalTable: "Paths",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_PathId",
+                table: "Files",
+                column: "PathId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paths_PathDataId",
+                table: "Paths",
+                column: "PathDataId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.DropTable(
+                name: "Paths");
+        }
+    }
+}
